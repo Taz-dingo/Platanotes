ca7c3840ee0af84bd991e1eb1736c76433bb2781
Taz-dingo <taste_my_blade@outlook.com>
2024-08-18 23:05:26 +0800

    docs: update web title and desc

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,12 @@ import Link from "next/link";
 import { getAllPosts } from "@/lib/posts";
 import "./globals.css";
 import Categories from "@/components/categories";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "梧桐树下",
+  description: "享受片刻宁静",
+};
 
 export default function RootLayout({
   children,
